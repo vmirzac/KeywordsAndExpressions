9fff7eb424ec6898bcdf9dcad44c48eb18238575
Victor Mirzac <Victor.Mirzac@endava.com>
2018-08-30 14:39:24 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int hightScore = 50;
+	if(hightScore ==50){
+        System.out.println("this is an expression");
+    }
+    }
+}
